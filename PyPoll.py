0a247f80625aa259d6fbe4c485b9d7a3b6b1c8c9
whartzler <109490755+whartzler@users.noreply.github.com>
2022-10-30 10:57:37 -0600

    PyPoll update with txt file creation

--- PyPoll.py
+++ PyPoll.py
@@ -47,30 +47,51 @@ with open(file_to_load) as election_data:
 
         #Calcualting the total votes for each candidate    
         candidate_votes[candidate_name] += 1
+    
+    #Open new txt file to write Election Data to
+    with open(file_to_save, "w") as txt_file:
 
-    #Loop to iterate through each candidate and calculate the votes for each
-    for candidate_name in candidate_votes:
+        #Formatting the Results of Election data and printing total votes
+        election_results = (
+            f"\nElection Results\n"
+            f"--------------------\n"
+            f"Total Votes: {total_votes:,}\n"
+            f"--------------------\n")
 
-        #assigning the total amount of votes to each candidate
-        votes = candidate_votes[candidate_name]
+        #Print Election Results to txt file and terminal
+        print(election_results, end="")
+        txt_file.write(election_results)
 
-        #Calculating the percentage of votes
-        vote_percentage = (votes / total_votes) * 100
+        #Loop to iterate through each candidate and calculate the votes for each
+        for candidate_name in candidate_votes:
 
-        print(f"{candidate_name}:{vote_percentage:.1f}% ({votes:,})\n")
+            #assigning the total amount of votes to each candidate
+            votes = candidate_votes[candidate_name]
 
-        #If statement to determine which Candidate has the most amount of votes and assign them as the Winner
-        if (votes > winning_count) and (vote_percentage > winning_percentage):
-            winning_count = votes
-            winning_percentage = vote_percentage
-            winning_candidate = candidate_name
+            #Calculating the percentage of votes
+            vote_percentage = float(votes) / float(total_votes) * 100
 
-    #Printing Winning Candidate summary information
-    winning_candidate_summary = (
-        f"-------------------------\n"
-        f"Winner: {winning_candidate}\n"
-        f"Winning Vote Count: {winning_count:,}\n"
-        f"Winning Percentage: {winning_percentage:.1f}%\n"
-        f"-------------------------\n")
+            #creating candidate results variable
+            candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
-    print(winning_candidate_summary)
+            #Printing the candidate results output to txt file and terminal
+            print(candidate_results)
+            txt_file.write(candidate_results)
+
+            #If statement to determine which Candidate has the most amount of votes and assign them as the Winner
+            if (votes > winning_count) and (vote_percentage > winning_percentage):
+                winning_count = votes
+                winning_percentage = vote_percentage
+                winning_candidate = candidate_name
+
+        #Printing Winning Candidate summary information
+        winning_candidate_summary = (
+            f"-------------------------\n"
+            f"Winner: {winning_candidate}\n"
+            f"Winning Vote Count: {winning_count:,}\n"
+            f"Winning Percentage: {winning_percentage:.1f}%\n"
+            f"-------------------------\n")
+
+        #Print Winning Candidate Summary to txt file and terminal
+        print(winning_candidate_summary)
+        txt_file.write(winning_candidate_summary)
